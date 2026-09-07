9e5ead4ab5134f4e1b91891bbef0035c51e78da5
M0hammedkhan <mohammedkhan8807@gmail.com>
2026-09-07 14:53:37 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,10 @@
+def p1(a, b):
+    print("Sum:", a + b)
+    print("Difference:", a - b)
+    print("Product:", a * b)
+    if b != 0:
+        print("Quotient:", a / b)
+        print("Remainder:", a % b)
+    else:
+        print("Quotient: undefined")
+        print("Remainder: undefined")
